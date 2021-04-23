6ecf00d4d0aa062a4bc0438d058b386a07922580
Matthew Ellis <mbellistx@gmail.com>
2021-04-23 13:50:18 -0500

    api/auth/register tests should be mostly done

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,55 @@
-// Write your tests here
+const request = require('supertest')
+
+const server = require('./server.js')
+const db = require('../data/dbConfig.js')
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).not.toBe(false)
+})
+
+beforeAll( async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+// beforeEach( async () => {
+//   await db.seed.run()
+// })
+
+describe('/api/auth', () => {
+  describe('/register', () => {
+    it('fails if username or password not provided', async () => {
+      let res = await request(server).post().send({})
+      expect(res.status).toBe(400)
+      expect(res.body.message).toBe('username and password required')
+      
+      res = await request(server).post().send({username: 'aoeu'})
+      expect(res.status).toBe(400)
+      expect(res.body.message).toBe('username and password required')
+      
+      res = await request(server).post().send({password: 'aoeu'})
+      expect(res.status).toBe(400)
+      expect(res.body.message).toBe('username and password required')
+    })
+    it('fails if username not unique', async () => {
+      let res = await request(server).post({ username: 'bobby', password: 'aoeu' })
+      expect(res.status).toBe(400)
+      expect(res.body.message).toBe('username taken')
+    })
+    it('sends new user back', async () => {
+      let res = await request(server).post({ username: 'newUser', password: 'aoeu' })
+      expect(res.status).toBe(201)
+      expect(res.body).toEqual({
+        id: 2,
+        username: 'newUser',
+        password: '....'
+      })
+    })
+    it('adds user to table', async () => {
+      await request(server).post({ username: 'newUser', password: 'aoeu' })
+      
+      let res = await request(server).post({ username: 'newUser', password: 'aoeu' })
+      expect(res.status).toBe(400)
+      expect(res.body.message).toBe('username taken')
+    })
+  }) 
 })
